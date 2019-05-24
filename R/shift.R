e2494de623f75a6294cc7368e4fcbc5eb8f33a47
Paul Harrison <paul.harrison@monash.edu>
2019-05-24 16:23:55 +1000

    Add some documentation

--- R/shift.R
+++ R/shift.R
@@ -72,6 +72,29 @@ counts_shift_inner <- function(counts, groups, min_reads) {
             per_read_weight = per_read_weights))
 }
 
+
+#' Produce a weitrix of shift scores
+#'
+#' Produce a weitrix of shift scores between -1 and 1. The input is read counts
+#' at a collection of peaks (or other features) in a collection of samples. The
+#' peaks can be grouped by gene, and are ordered within each gene.
+#' 
+#' For a particular gene, a shift score measures measures the tendency of reads to be upstrand (negative) or downstrand (positive) of the average over all samples. Shift scores range between -1 and 1. 
+#' 
+#' @param counts A matrix of read counts. Rows are peaks and columns are samples.
+#' 
+#' @param grouping A data frame defining the grouping of peaks into genes. Should have a column "group" naming the gene and a column "name" naming the peak (corresponding to \code{rownames(counts)}). Within each group, peak names should be ordered from 5' to 3' position.
+#' 
+#' @param min_reads Minimum reads to produce a shift score. Where there are fewer than this many reads for a combination of gene and sample, a weight of zero is given.
+#' 
+#' @param bio_var Produce weights that allow for biological variation.
+#' 
+#' @param design For biological variation estimation. A design matrix for a linear model, which could account for batches or experimental design. Leaving this with its default will give a conservative choice of weights, and should be safe.
+#' 
+#' @param p For biological variation estimation. Attempt to find p further components of variation, using \code{weitrix_components}.
+#' 
+#' @param verbose If TRUE, output some debugging and progress information.
+#'
 #' @export
 counts_shift <- function(counts, grouping, min_reads=1, biovar=TRUE, design=~0, p=0, verbose=TRUE) {
     assert_that(
@@ -98,7 +121,7 @@ counts_shift <- function(counts, grouping, min_reads=1, biovar=TRUE, design=~0, 
     if (biovar) {
         if (verbose)
             message("Calculating biological noise component")
-        result <- weight_balance(result, rowData(result)$per_read_weight, design=design, p=p)
+        result <- weight_balance(result, rowData(result)$per_read_weight, design=design, p=p, verbose=verbose)
         metadata(result)$weitrix$effective_max_reads <- 
             metadata(result)$weitrix$tech_scale / metadata(result)$weitrix$bio_scale
     }
